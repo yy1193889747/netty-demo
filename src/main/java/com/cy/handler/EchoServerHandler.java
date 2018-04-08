67e84f31f6cd24bb9c2a2f6700be2b0b3144f25c
yy1193889747 <1193889747@qq.com>
2018-04-08 14:15:17 +0800

    echo server

---
+++ src/main/java/com/cy/handler/EchoServerHandler.java
@@ -0,0 +1,31 @@
+package com.cy.handler;
+
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * @author ocly
+ * @date 2018/4/8 13:40
+ */
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("Server received: " + msg);
+        ctx.write(msg);
+    }
+
+
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
---
+++ src/main/java/com/cy/server/EchoNettyServer.java
@@ -0,0 +1,51 @@
+package com.cy.server;
+
+import com.cy.handler.EchoServerHandler;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+
+/**
+ * @author ocly
+ * @date 2018/4/8 11:49
+ */
+public class EchoNettyServer {
+
+    private final int port;
+
+    public EchoNettyServer(int port) {
+        this.port = port;
+    }
+
+    public void start() throws Exception {
+
+        EventLoopGroup boss = new NioEventLoopGroup();
+        EventLoopGroup worker = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(worker).channel(NioServerSocketChannel.class).localAddress(port)
+                    .childHandler(new ChannelInitializer<Channel>() {
+                        @Override
+                        protected void initChannel(Channel channel) throws Exception {
+                            channel.pipeline().addLast(new EchoServerHandler());
+                        }
+                    });
+            ChannelFuture f = b.bind().sync();
+            System.out.println(EchoNettyServer.class.getName() + "started and listen on " + f.channel().localAddress());
+            f.channel().closeFuture().sync();
+
+        }finally {
+            worker.shutdownGracefully();
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        new EchoNettyServer(9999).start();
+    }
+}
